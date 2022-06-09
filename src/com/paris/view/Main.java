4d5b18f86712c4dc2d07a7f0ec01866db475324b
= <=>
2022-06-09 14:04:42 +0430

    add ATM.SQL for this project

--- src/com/paris/view/Main.java
+++ src/com/paris/view/Main.java
@@ -1,12 +1,16 @@
 package com.paris.view;
 
+import com.paris.controller.AccountController;
 import com.paris.controller.PersonController;
 
 public class Main {
     public static void main(String[] args) {
+/*
         PersonController personController = new PersonController();
         personController.save("0024562900", "Shirin", "Jalilvand", 24);
         personController.save("0014371715", "Milad", "Tehrani", 300);
+*/
+
 
     }
 }
